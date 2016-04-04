dd14f0e27110e549b1e6dfe2e16d5056ea4b472e
Yotam <yotam.roth@sisense.com>
2016-04-04 10:44:43 +0300

    upload files

---
+++ clientMessenger.js
@@ -0,0 +1,41 @@
+var io = require('engine.io-client');
+
+var _addr = window.location.protocol + '//' + window.location.host;
+
+var socket;
+
+function connect() {
+	socket = new io.Socket(_addr, {
+	    transports: ['polling']
+	});
+
+	socket.on('close', function() {
+		console.log('Socket to server was closed.');
+	})
+}
+
+function subscribe(eventName, callback) {
+	var registerEvent = {
+		eventName: '_registerEvent',
+		data: eventName
+	};
+
+	socket.send(registerEvent);
+
+	socket.on('message', function(data) {
+		var dataObject = JSON.parse(data);
+
+		if (dataObject.eventName == eventName) {
+			callback.call(null, dataObject.data);
+		}
+	})
+}
+
+function send(eventName, data) {
+
+}
+
+module.exports.connect = connect;
+module.exports.subscribe = subscribe;
+module.exports.send = send;
+
